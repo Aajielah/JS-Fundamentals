0e014e04140eb5ba23e2d0c4edc0db3f7e120c06
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2025-11-10 12:12:07 +0100

    Arguments found

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+let Myarguments = 2;
+
+if (Myarguments === 0) {
+  console.log("No argument");
+}
+else if (Myarguments === 1) {
+  console.log("Argument found");
+}
+else if (Myarguments === 2) {
+  console.log("Arguments found");
+}
